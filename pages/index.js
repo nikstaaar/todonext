48fb137fb2e0eed3d7c8125319bab697224603d7
Niklas Ostwald <niktjawalla922@gmail.com>
2022-09-09 15:31:09 +0200

    add list of todos from useState

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,39 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
+
 export default function Home() {
+
+const [todos, setTodos] = useState ([
+  { description: "clean my unterhose",
+  done: false,
+  id: nanoid()},
+  { description: "clean my teeth",
+  done: false,
+  id: nanoid()}
+])
+
   return (
     <div>
+      <div>
       <h1>TODO APP</h1>
       <h3>TYPE YOUR TODOS HERE</h3>
       <input type="text"></input>
       <h3>NOCH ZU ERLEDIGEN:</h3>
-      <p>Programmieren</p>
+      </div>
+      <div>
+      {todos.map((todo)=>{
+        return(
+          <div>
+      <p>{todo.description}</p>
       <button>Delete</button>
+      </div>
+        )
+      })}
+      </div>
+
+      
+
+
       <h3>ERLEDIGT:</h3>
       <p>Kochen</p>
       <button>Delete</button>
