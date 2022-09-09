d1498e827971677587a43c24312a7b4845a00d1f
Niklas Ostwald <niktjawalla922@gmail.com>
2022-09-09 16:10:13 +0200

    add delete button

--- pages/index.js
+++ pages/index.js
@@ -3,36 +3,38 @@ import { nanoid } from "nanoid";
 
 export default function Home() {
 
-const [todos, setTodos] = useState ([
-  { description: "clean my unterhose",
-  done: false,
-  id: nanoid()},
-  { description: "clean my teeth",
-  done: false,
-  id: nanoid()}
-])
+const [input, setInput] = useState ("")
+const [todos, setTodos] = useState ([])
 
   return (
     <div>
       <div>
       <h1>TODO APP</h1>
       <h3>TYPE YOUR TODOS HERE</h3>
-      <input type="text"></input>
+      <input type="text" value = {input} onChange = {((event)=>{
+        setInput(event.target.value)
+      })}></input>
+      <button onClick = {(()=>{
+        setTodos([...todos, {
+          description: input,
+          done: false,
+          id: nanoid(),
+        }])
+      })}>add</button>
       <h3>NOCH ZU ERLEDIGEN:</h3>
       </div>
       <div>
       {todos.map((todo)=>{
         return(
-          <div>
+          <div key = {todo.id}>
       <p>{todo.description}</p>
-      <button>Delete</button>
+      <button onClick = {(()=>{
+      setTodos(todos.filter(todos => todos.id !== todo.id))
+      })}>Delete</button>
       </div>
         )
       })}
       </div>
-
-      
-
 
       <h3>ERLEDIGT:</h3>
       <p>Kochen</p>
